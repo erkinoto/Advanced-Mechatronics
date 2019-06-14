223fd864f6ae5e9067f3ea67eb8a641a9887a43e
erkinoto <49204502+erkinoto@users.noreply.github.com>
2019-06-14 16:34:00 -0500

    HW15
    HW 15 done

---
+++ HW15a.X/LSM6DS33.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   LSM6DS33.h
+ * Author: erkin
+ *
+ * Created on June 14, 2019, 2:09 PM
+ */
+
+#ifndef I2C_IMU_H__
+#define I2C_IMU_H__
+// Header file for i2c_master_noint.c
+// helps implement use I2C1 as a master without using interrupts
+void initIMU(void);
+void I2C_read_multiple(unsigned char address, unsigned char reg, unsigned char * data, int length);
+char I2C_read(unsigned char address, unsigned char reg);
+
+
+void setExpander(char pin, char level);
+char getExpander();
+
+
+
+#endif
---
+++ HW15a.X/build/default/production/LSDM6DS33.o.d
@@ -0,0 +1,3 @@
+build/default/production/LSDM6DS33.o:  \
+LSDM6DS33.c  \
+i2c_master_noint.h 
